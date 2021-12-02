616441a5351ca16d04e085ff25f048d1dae5f98a
houstonlucas <houstonlucas@nevada.unr.edu>
2021-12-01 22:26:00 -0800

    Added config and updated shutdown procedure in factorio_monitor.py.

--- src/factorio_monitor.py
+++ src/factorio_monitor.py
@@ -10,13 +10,15 @@ from pathlib import Path
 sys.path.append(".")
 
 from src.server_monitor import GameMonitor, EC2ServerMonitor
-from src.utils import tmux_sendkeys
+from src.utils import tmux_sendkeys, json_from_file
 
 
 class FactorioMonitor(GameMonitor):
-    def __init__(self, save_file: str, debug_mode: bool = False):
+    def __init__(self, config_file: str, debug_mode: bool = False):
         super().__init__(debug_mode)
-        self.save_file = save_file
+        # TODO: don't pass save file in directly, pass in a config file
+        self.config = json_from_file(config_file)
+        self.save_file = self.config['save_file']
         self.tmux_log = "../logs/tmux_factorio.log"
         assert (pathlib.Path(self.save_file).exists())
         self.port = 34197
@@ -30,7 +32,9 @@ class FactorioMonitor(GameMonitor):
 
     def shutdown_game_server(self):
         # Issue commands to the tmux session
-        tmux_sendkeys(self.tmux_session_name, "/quit")
+        tmux_sendkeys(self.tmux_session_name, '/save')
+        time.sleep(5.0)
+        tmux_sendkeys(self.tmux_session_name, '/quit')
 
     def parse_command(self, data: bytes):
         # TODO: actually parse command. This is for testing.
